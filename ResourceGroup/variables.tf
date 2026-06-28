af77f6b4c27111fdcc00675bb60ca6ff3ce5164d
anjan_gn <143089310+gn-anjan@users.noreply.github.com>
2026-06-28 15:55:19 +0530

    updated

--- ResourceGroup/variables.tf
+++ ResourceGroup/variables.tf
@@ -1,11 +1,28 @@
 variable "RSG_NAME" {
   type = string
+  default = "Test"
+  description = "ResourceGroupName"
 }
 
 variable "RSG_LOCATION" {
   type = string
+  default = "Test"
+  description = "ResourceGroupLocation"
 }
 
 variable "tags" {
-  type    = map(string)
+  type = map(string)
+
+  default = {
+    Environment = "dev"
+    Project     = "Demo"
+  }
+
+  description = "Resource Group tags"
+}
+
+variable "subscription_id" {
+  type = string
+  default = "Test"
+  description = "SubscriptionID"
 }
